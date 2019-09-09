acaf3a1a616a8ac4e5927eeff6553360f2efef68
Ryan Yeo <ryanrj@hotmail.sg>
2019-09-09 20:08:40 +0800

    Created more game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,27 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+var card = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = card[0];
+var cardTwo = card[1];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo); 
+
+console.log("User flipped " + cardsInPlay[0]);
+console.log("User flipped " + cardsInPlay[1]);
+
+console.log(cardsInPlay);
+
+if(cardsInPlay.length === 2) {
+	if(cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You've found a match!")
+	} else {
+		alert("Sorry, try again")
+	}
+}
+
+
