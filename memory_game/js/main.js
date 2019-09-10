c9702970024e9e627e5dbb1b1c36b86e5d05c3fd
Ryan Yeo <ryanrj@hotmail.sg>
2019-09-10 21:03:49 +0800

    create even more more game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -5,23 +5,28 @@ console.log("Up and running!");
 var card = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = card[0];
-var cardTwo = card[1];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo); 
-
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
-
-console.log(cardsInPlay);
-
-if(cardsInPlay.length === 2) {
+function checkForMatch() {
 	if(cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You've found a match!")
+		console.log("You've found a match!");
 	} else {
-		alert("Sorry, try again")
+		console.log("Sorry, try again");
 	}
-}
+};
+
+function flipCard(cardId){
+
+	console.log("User flipped " + card[cardId]);
+	cardsInPlay.push(card[cardId]);
+
+	if(cardsInPlay.length === 2) {
+		checkForMatch();
+	};
+
+};
+
+flipCard(0);
+flipCard(1);
+
+
 
 
